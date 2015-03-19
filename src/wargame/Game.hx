28fbc60c0cf038d5632a1d256f3a1dc40332b8b2
Michael Martin-Smucker <mlmartin13@gmail.com>
2015-03-18 23:01:15 -0600

    A world and renderer, kind of

--- src/wargame/Game.hx
+++ src/wargame/Game.hx
@@ -1,17 +1,26 @@
 package wargame;
 
+import edge.World;
+import edge.Engine;
 import pixi.core.display.Container;
 import pixi.core.renderers.SystemRenderer;
 
 class Game {
   var stage : Container;
   var renderer : SystemRenderer;
+  var world : World;
+  var engine : Engine;
 
   public function new(stage : Container, renderer : SystemRenderer) {
     this.stage = stage;
     this.renderer = renderer;
+
+    world = new World();
+    engine = world.engine;
+
+    world.render.add(new wargame.render.PixiRenderer(stage, renderer));
   }
   public function start() {
-
+    world.start();
   }
 }
---
+++ src/wargame/render/PixiRenderer.hx
@@ -0,0 +1,19 @@
+package wargame.render;
+
+import edge.ISystem;
+import pixi.core.renderers.SystemRenderer;
+import pixi.core.display.Container;
+
+class PixiRenderer implements ISystem {
+  var stage : Container;
+  var renderer : SystemRenderer;
+
+  public function new(stage : Container, renderer : SystemRenderer) {
+    this.stage = stage;
+    this.renderer = renderer;
+  }
+
+  public function update() {
+    /*renderer.render(stage);*/
+  }
+}
